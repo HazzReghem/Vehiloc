7774f8d5fd762241dedd39b8b30c98032b07e8cf
Thibault Roussel <thibault.roussel@gmx.com>
2024-11-13 10:33:25 +0100

    Twig for home page & CarsController added

---
+++ src/Controller/CarsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+use App\Repository\CarsRepository;
+
+class CarsController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(CarRepository $carRepository): Response
+    {
+        // Récupération des voitures grâce au Repository
+        $cars = $carRepository->findAll();
+
+        return $this->render('cars/index.html.twig', [
+            'controller_name' => 'CarsController',
+        ]);
+    }
+}
