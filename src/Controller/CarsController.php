404463b086df90b210e790f2f76b5bfe4095521c
Thibault Roussel <thibault.roussel@gmx.com>
2024-11-14 10:42:06 +0100

    delete button now works

--- src/Controller/CarsController.php
+++ src/Controller/CarsController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 use App\Repository\CarRepository;
+use App\Entity\Car;
 
 class CarsController extends AbstractController
 {
@@ -34,5 +37,23 @@ class CarsController extends AbstractController
         return $this->render('cars/car.html.twig', [
             'car' => $car,
         ]);
+    }
+
+    #[Route('/car/{id}/delete', name: 'app_car_delete', methods:['POST'])]
+    public function delete(int $id, CarRepository $carRepository, EntityManagerInterface $entityManager): RedirectResponse
+    {
+        $car  = $carRepository->find($id);
+
+        if (!$car) {
+            throw $this->createNotFoundException("Nous ne parvenons pas à trouver cette voiture !");
+        }
+
+        // Suppression de la voiture
+        $entityManager->remove($car);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'La voiture a été supprimée avec succès !');
+        
+        return $this->RedirectToRoute('app_home');
     }
 }
